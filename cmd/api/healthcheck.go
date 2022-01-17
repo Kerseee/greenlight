11ab1159b373d6cf60b334b4f0a774925ff87a7b
黃楷翔 <huangkaixiang@huangkaixiangdeMacBook-Air.local>
2022-01-17 21:01:52 +0800

    Update healthcheckHandler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,13 @@ import (
 
 // healthcheckHandler shows the health status to client.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed format JSON message.
+	js := `{"status": "available", "environment": %q, "version": "%q"}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set the "Content-Type" response header to JSON.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the response body.
+	w.Write([]byte(js))
 }
