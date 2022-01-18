620426ff4c45cdd72a33ff46a497d5afbb642920
黃楷翔 <huangkaixiang@huangkaixiangdeMacBook-Air.local>
2022-01-18 22:59:54 +0800

    Encoding JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // healthcheckHandler shows the health status to client.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a fixed format JSON message.
-	js := `{"status": "available", "environment": %q, "version": "%q"}`
-	js = fmt.Sprintf(js, app.config.env, version)
 
-	// Set the "Content-Type" response header to JSON.
-	w.Header().Set("Content-Type", "application/json")
+	data := map[string]string{
+		"status":      "available",
+		"environment": "somthing",
+		"version":     version,
+	}
 
-	// Write the response body.
-	w.Write([]byte(js))
+	// Write JSON.
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Server Error! Cannot write JSON!", http.StatusInternalServerError)
+	}
+
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -21,4 +22,32 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// writeJSON is a helper for sending responses in JSON.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Encode the data to JSON.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Append a newline.
+	js = append(js, '\n')
+
+	// Add the headers into the response header.
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+
+	// Set the "Content-Type" header to JSON.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the response header.
+	w.WriteHeader(status)
+
+	// Write the response body.
+	w.Write(js)
+
+	return nil
 }
